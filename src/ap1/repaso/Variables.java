19d98d531f737bb20cfc08af13b7c95daf9bf43f
adriagriera-upm <adria.griera@alumnos.upm.es>
2026-09-16 16:34:46 +0200

    Initial commit. Hechos ej 1 y 3

---
+++ src/ap1/repaso/Variables.java
@@ -0,0 +1,43 @@
+package ap1.repaso;
+
+public class Variables {
+	public static void main(String[] args) {
+		
+		/**
+		 * Ej 1
+		 */
+		short diasAno=365;
+		char horaDias=24;
+		char minHora=60;
+		char segMin=minHora;
+		
+		int segAno=diasAno*horaDias*minHora*segMin;
+		
+		System.out.println(segAno);
+		
+		/**
+		 * Ej 3
+		 */
+		int suma=0;
+		int contador=0;
+		for(int i=1;i<101;i++) {
+			if(i%5==0) {
+				suma+= i;
+				contador++;
+				System.out.println("Múltiplo de 5 número " + contador + ": " + i);
+				}
+				
+			}
+		System.out.println(suma);
+		
+		/**
+		 * Ej 5
+		 */
+	
+	String.
+	
+	}
+	
+		
+}
+
---
+++ src/ap5/repaso/Punto3D.java
@@ -0,0 +1,28 @@
+package ap5.repaso;
+
+public class Punto3D {
+	
+	int origenX;
+	int origenY;
+	int origenZ;
+	
+	int coordX;
+	int coordY;
+	int coordZ;
+	
+	public Origen(int origenX, int origenY, int origenZ){
+		this.origenX=origenX;
+		this.origenY=origenY;
+		this.origenZ=origenZ;
+		
+	}
+	
+	public Punto3D(int coordX, int coordY, int coordZ) {
+		this.coordX=coordX;
+		this.coordY=coordY;
+		this.coordZ=coordZ;
+		
+	}
+	
+
+}
