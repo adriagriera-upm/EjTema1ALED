97f992d956e4700851ada2cf02b0baee568d1f30
adriagriera-upm <adria.griera@alumnos.upm.es>
2026-09-22 19:36:41 +0200

    Algunos ejercicios de manejo de archivos

--- src/Archivos/FileModifier.java
+++ src/Archivos/FileModifier.java
@@ -1,31 +1,60 @@
 package Archivos;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class FileModifier {
 
-		/*
-		 * Ejercicio 1
-		 */
+	/*
+	 * Ejercicio 1
+	 */
 	public static void readFile(String filename) {
 		
 	
 		try{
-			FileInputStream fis = new FileInputStream(filename);
-			PrintStream ps = new PrintStream(filename);
-			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			String linea;
+			while ((linea =br.readLine()) != null) {
+			    System.out.println(linea);
+			}
+			br.close();
 	
 			
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Archivo no detectado");
 		}
 	}
+	
+	public static void saveFloatArray(String filename, float[] newfloat) {
+	    try{
+	    	BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+	        for (float f : newfloat) {
+	            bw.write(Float.toString(f));
+	            bw.write(", ");
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	
 		
 	public static void main(String[] args) {
-		
+		/*
+		 *Ejercicio 1 y 2
+		 */
+		float[] newf = {1,3,4,6,63,13,5,32,4};
+		saveFloatArray("textfile.txt",newf);
 		readFile("textfile.txt");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
 	
 	}
+	
 
 
 
